da2047d3e6c195767e0fae702fd9b031adcf882f
madprops/mc <manuelchaves@gmail.com>
2017-08-03 04:37:22 -0600

    min

--- prepare.bat
+++ prepare.bat
@@ -1,2 +1,2 @@
-call uglifyjs .\docs\msg.js --compress --mangle --comments all --output .\dist\msg-min.js
+call uglifyjs .\docs\msg.js --compress --mangle --comments all --output .\dist\msg.min.js
 call xcopy .\docs\msg.js .\dist\ /Y
